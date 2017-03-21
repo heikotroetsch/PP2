87b0b767fa70baa1cb8616d51747a5f1bab42051
seife96 <heiko@troetsch.net>
2017-03-21 13:12:56 +0100

    a) 2

--- P1_TroetschHeiko/src/model/AnswerList.java
+++ P1_TroetschHeiko/src/model/AnswerList.java
@@ -1,29 +1,72 @@
 package model;
 
+import java.util.Arrays;
 import java.util.Vector;
 
-public class AnswerList implements AnswerCollection{
+class AwnserVocable{
+	protected String awnser;
+	protected int mode;
+	
+	protected AwnserVocable(String awnser, int mode){
+		this.awnser = awnser;
+		this.mode = mode;
+	}
+}
 
+public class AnswerList implements AnswerCollection{
+	
+	private Vector<AwnserVocable> vocableAnswers;
+	public static final int SOURCE_GIVEN=0,TARGET_GIVEN=1;
+	double lernfortschritt = 100;
+	
 	public AnswerList (int min){
 		
 	}
 	
 	@Override
 	public boolean addAnswer(String word, int mode) {
-		// TODO Auto-generated method stub
-		return false;
+		this.vocableAnswers.add(new AwnserVocable(word, mode));
+		return true;
 	}
 
 	@Override
 	public void check(Vector<Vocable> vocables) {
-		// TODO Auto-generated method stub
+		double fehler = 100/vocableAnswers.size();
+		//goes through all words in the lists
+		for(int i = 0; i<vocableAnswers.size(); i++){
+			//if the mode of the typed word was with the target given
+			if(vocableAnswers.elementAt(i).mode == TARGET_GIVEN){
+				//compares awnser to source word
+				lernfortschritt -= compareWords(vocableAnswers.elementAt(i).awnser, vocables.elementAt(i).getWord(), fehler);
+			}else if(vocableAnswers.elementAt(i).mode == SOURCE_GIVEN){
+				//compares awnser to all translations and selects the smallest
+				double temp[] = new double[vocables.elementAt(i).getTranslations().size()];
+				for(int p = 0; p<vocables.elementAt(i).getTranslations().size() ;p++){
+					temp[p] = compareWords(vocableAnswers.elementAt(i).awnser, vocables.elementAt(i).getTranslations().get(p), fehler);
+				}
+				Arrays.sort(temp);
+				lernfortschritt -= temp[0];
+			}
+		}
 		
 	}
 
+	
+	public double compareWords(String wordOne, String wordTwo, double bigFehler){
+		double result = 0;
+			if(wordOne.toLowerCase().matches(wordTwo.toLowerCase())){
+				result -= bigFehler/2;
+				return result;
+			}else{
+				result -= bigFehler;
+					result += bigFehler* (Math.abs(wordOne.compareTo(wordTwo)) / wordOne.length());
+				return result;
+			}
+	}
+	
 	@Override
 	public int getCurrentIndex() {
-		// TODO Auto-generated method stub
-		return 0;
+		return vocableAnswers.size()+1;
 	}
 
 }
--- P1_TroetschHeiko/src/model/LearnSettings.java
+++ P1_TroetschHeiko/src/model/LearnSettings.java
@@ -3,7 +3,16 @@ package model;
 import java.util.ArrayList;
 
 import view.LearnGUI;
-
+/**
+ * Class for storing, changing and looking up Learn Context Settings.
+ * @author heikotroetsch
+ * 
+ * @param chosenUnits is a parameter which contains all the Units from which vocab words are selected. 
+ * @param choseSections is a parameter which contains all the Sections from which vocab words are selected.
+ * @param MaxAnzahlWokabln is the maximum number of vocab words that are in included in one learn session.
+ * @param learnMode selects the Mode in wich the vocab words are asked in. (SOURCE_GIVEN=0,TARGET_GIVEN=1, MIXED=2)
+ *
+ */
 public class LearnSettings implements LearningContext{
 
 	public static final int SOURCE_GIVEN=0,TARGET_GIVEN=1, MIXED=2;
@@ -12,7 +21,14 @@ public class LearnSettings implements LearningContext{
 	private int MaxAnzahlVokabeln;
 	private int learnMode;
 	
-
+	/**
+	 * Constructor for the creation of one Object from the class LearnSettings. 
+	 * @param chosenUnits is a parameter which contains all the Units from which vocab words are selected. 
+	 * @param choseSections is a parameter which contains all the Sections from which vocab words are selected.
+	 * @param MaxAnzahlWokabln is the maximum number of vocab words that are in included in one learn session.
+	 * @param learnMode selects the Mode in wich the vocab words are asked in. (SOURCE_GIVEN=0,TARGET_GIVEN=1, MIXED=2)
+	 *
+	 */
 	public LearnSettings(ArrayList<Integer> chosenUnits,ArrayList<Integer> chosenSections,int MaxAnzahlVokabeln, int learnMode){
 		this.chosenUnits = chosenUnits;
 		this.chosenSections = chosenSections;
@@ -20,6 +36,11 @@ public class LearnSettings implements LearningContext{
 		this.learnMode = learnMode;
 	}
 	
+	/**
+	 * This method returns a boolean true or false. True if the Vocab word give matches the learn settings of the object. This method specifically checks if unit and section are correct.
+	 *@param v will be a vocable which is checked if it fits into the learning context.
+	 *@return True if the parameter v matches the learning context and false if not.
+	 */
 	@Override
 	public boolean matches(Vocable v) {
 		boolean isInUnit = false;
@@ -40,6 +61,10 @@ public class LearnSettings implements LearningContext{
 		return false;
 	}
 
+	/**
+	 * This method returns the current Learn Mode that the programm will go through.
+	 * @return int SOURCE_GIVEN=0,TARGET_GIVEN=1, MIXED=2
+	 */
 	@Override
 	public int getMode() {
 		if(this.learnMode==LearnGUI.SOURCE_GIVEN||this.learnMode==LearnGUI.TARGET_GIVEN){
@@ -49,6 +74,9 @@ public class LearnSettings implements LearningContext{
 		}
 	}
 
+	/**
+	 * @return Max vocab words that are in the learn context.
+	 */
 	@Override
 	public int getSize() {
 		return this.MaxAnzahlVokabeln;
